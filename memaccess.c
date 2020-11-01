6d04e860448a6a7c4deaeb5c53eb8c9e901ae274
Felix Pojtinger <felix@pojtinger.com>
2020-11-01 21:58:10 +0000

    feat: Add example of transfering struct between C and WASM

--- memaccess.c
+++ memaccess.c
@@ -1,5 +1,15 @@
 #include "memaccess.h"
+#include <arpa/inet.h>
+#include <netinet/in.h>
 #include <stdio.h>
+
+#define REMOTE_ADDR "127.0.0.1"
+#define REMOTE_PORT 6912
+
+struct test_struct {
+  char *my_string;
+  int my_int;
+};
 
 int main() {
   int my_positive_int = 239234;
@@ -17,6 +27,36 @@ int main() {
 
   transfer_to_runtime(my_negative_int, &my_negative_int,
                       sizeof(my_negative_int));
+
+  printf("size before filling=%lu\n", sizeof(struct test_struct));
+
+  struct test_struct my_test_struct;
+  my_test_struct.my_string = "asdf";
+  my_test_struct.my_int = 234234;
+
+  printf("my_string=%s my_int=%d\n", my_test_struct.my_string,
+         my_test_struct.my_int);
+
+  printf("size after filling=%lu\n", sizeof(struct test_struct));
+
+  printf("before=%d after=%d\n", REMOTE_PORT, htons(REMOTE_PORT));
+
+  struct sockaddr_in server_addr;
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(REMOTE_PORT);
+  if (inet_pton(AF_INET, REMOTE_ADDR, &server_addr.sin_addr) == -1) {
+    perror("inet_pton");
+
+    exit(-1);
+  }
+  socklen_t server_socket_length = sizeof(struct sockaddr_in);
+
+  printf("server_addr=%lu sin_port=%lu sin_family=%lu sin_addr=%lu\n",
+         sizeof(server_addr), sizeof(server_addr.sin_port),
+         sizeof(server_addr.sin_family),
+         sizeof(server_addr.sin_addr)); // The rest are 8 bytes of zero
+
+  transfer_to_runtime_sockaddr_in(&server_addr, server_socket_length);
 
   return 0;
 }
--- memaccess.h
+++ memaccess.h
@@ -1,3 +1,5 @@
+#include <netinet/in.h>
 #include <stdlib.h>
 
 int transfer_to_runtime(int, int *, size_t);
+int transfer_to_runtime_sockaddr_in(struct sockaddr_in *, size_t);
