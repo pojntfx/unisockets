03ab3303e30e604de2461b46d8d42bf143f2f592
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-11-02 19:47:51 +0000

    feat: Add stubs for basic client berkeley sockets functions

--- src/client_example.c
+++ src/client_example.c
@@ -10,37 +10,37 @@
 
 int main() {
   // Variables
-  int server_sock;
-  struct sockaddr_in server_addr;
+  int remote_sock;
+  struct sockaddr_in remote_addr;
 
   ssize_t sent_message_length;
   char sent_message[SENT_MESSAGE_MAX_LENGTH];
   size_t received_message_length;
   char received_message[RECEIVED_MESSAGE_MAX_LENGTH];
 
-  socklen_t server_socket_length = sizeof(struct sockaddr_in);
+  socklen_t remote_addr_length = sizeof(struct sockaddr_in);
 
-  memset(&server_addr, 0, sizeof(server_addr));
+  memset(&remote_addr, 0, sizeof(remote_addr));
 
   // Create address to connect to
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_port = htons(REMOTE_PORT);
-  if (inet_pton(AF_INET, REMOTE_ADDR, &server_addr.sin_addr) == -1) {
+  remote_addr.sin_family = AF_INET;
+  remote_addr.sin_port = htons(REMOTE_PORT);
+  if (inet_pton(AF_INET, REMOTE_ADDR, &remote_addr.sin_addr) == -1) {
     perror("inet_pton");
 
     exit(-1);
   }
 
   // Create socket
-  if ((server_sock = berkeley_sockets_socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+  if ((remote_sock = berkeley_sockets_socket(AF_INET, SOCK_STREAM, 0)) == -1) {
     perror("socket");
 
     exit(-1);
   }
 
   // Connect
-  if ((berkeley_sockets_connect(server_sock, (struct sockaddr *)&server_addr,
-                                server_socket_length)) == -1) {
+  if ((berkeley_sockets_connect(remote_sock, (struct sockaddr *)&remote_addr,
+                                remote_addr_length)) == -1) {
     perror("connect");
 
     exit(-1);
@@ -53,12 +53,12 @@ int main() {
     fgets(sent_message, SENT_MESSAGE_MAX_LENGTH, stdin);
 
     // Send
-    sent_message_length = berkeley_sockets_send(server_sock, sent_message,
+    sent_message_length = berkeley_sockets_send(remote_sock, sent_message,
                                                 strlen(sent_message), 0);
 
     // Receive
     received_message_length = berkeley_sockets_recv(
-        server_sock, &received_message, RECEIVED_MESSAGE_MAX_LENGTH, 0);
+        remote_sock, &received_message, RECEIVED_MESSAGE_MAX_LENGTH, 0);
 
     printf("%s", received_message);
   }
--- src/client_example.test.js
+++ src/client_example.test.js
@@ -5,11 +5,35 @@ import { WASI } from "wasi";
   const wasi = new WASI();
 
   const wasm = await WebAssembly.compile(
-    fs.readFileSync(__dirname + "/client_example.wasm")
+    fs.readFileSync("./src/client_example.wasm")
   );
 
   const instance = await WebAssembly.instantiate(wasm, {
     wasi_snapshot_preview1: wasi.wasiImport,
+    env: {
+      berkeley_sockets_socket: (family, stream, option) => {
+        return -1;
+      },
+      berkeley_sockets_connect: (socket, addressPointer, addressLength) => {
+        return -1;
+      },
+      berkeley_sockets_send: (
+        socket,
+        messagePointer,
+        messagePointerLength,
+        option
+      ) => {
+        return -1;
+      },
+      berkeley_sockets_recv: (
+        socket,
+        messagePointer,
+        messagePointerLength,
+        option
+      ) => {
+        return -1;
+      },
+    },
   });
 
   wasi.start(instance);
