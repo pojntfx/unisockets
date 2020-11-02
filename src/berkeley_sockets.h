b2faca0b3dde26643b5bcdfd989cc9d71af5b5ba
Felix Pojtinger <felix@pojtinger.com>
2020-11-02 23:35:56 +0000

    feat: Enable compilation of socket client on WASM and native platforms

--- src/berkeley_sockets.h
+++ src/berkeley_sockets.h
@@ -1,10 +1,16 @@
+#ifndef BERKELEY_SOCKETS_H
+#define BERKELEY_SOCKETS_H
+
 #include <arpa/inet.h>
-#include <netinet/in.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 int berkeley_sockets_socket(int, int, int);
 int berkeley_sockets_connect(int, const struct sockaddr *, socklen_t);
 ssize_t berkeley_sockets_send(int, const void *, size_t, int);
 ssize_t berkeley_sockets_recv(int, void *, size_t, int);
+
+#define socket berkeley_sockets_socket
+#define connect berkeley_sockets_connect
+#define send berkeley_sockets_send
+#define recv berkeley_sockets_recv
+
+#endif /* BERKELEY_SOCKETS_H */
