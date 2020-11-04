8b357889233089e71c732021943d8250612b6851
Felix Pojtinger <felix@pojtinger.com>
2020-11-04 21:57:02 +0000

    feat: Start implementation of `bind`, `listen` and `accept` bindings

--- src/berkeley_sockets.js
+++ src/berkeley_sockets.js
@@ -9,7 +9,9 @@ export class BerkeleySockets {
       stream,
       option,
       (message, option) => this.handleSend(fd, message, option),
-      (option) => this.handleRecv(fd, option)
+      (option) => this.handleRecv(fd, option),
+      (maxClients) => this.handleListen(fd, maxClients),
+      () => this.handleAccept(fd)
     );
 
     this.#sockets.push([newSocket, []]);
@@ -23,6 +25,16 @@ export class BerkeleySockets {
 
   htons(val) {
     return ((val & 0xff) << 8) | ((val >> 8) & 0xff);
+  }
+
+  getUint2(val) {
+    const sinVal = new ArrayBuffer(4);
+
+    new Int32Array(sinVal)[0] = val;
+
+    const outVal = Array.from(new Uint8Array(sinVal)).reverse();
+
+    return [outVal[2], outVal[3]];
   }
 
   handleSend(fd, message, option) {
@@ -44,6 +56,18 @@ export class BerkeleySockets {
 
     return message;
   }
+
+  handleListen(fd, maxClients) {
+    // TODO: Connect remote API
+  }
+
+  handleAccept(fd) {
+    return {
+      family: 1,
+      port: 1234,
+      address: "127.0.0.1",
+    };
+  }
 }
 
 export class Socket {
@@ -53,25 +77,33 @@ export class Socket {
 
   #onSend = () => {};
   #onRecv = () => {};
+  #onListen = () => {};
+  #onAccept = () => {};
 
   #port = 0;
   #addr = "";
 
-  constructor(family, stream, option, onSend, onRecv) {
+  constructor(family, stream, option, onSend, onRecv, onListen, onAccept) {
     this.#family = family;
     this.#stream = stream;
     this.#option = option;
 
     this.#onSend = onSend;
     this.#onRecv = onRecv;
+    this.#onListen = onListen;
+    this.#onAccept = onAccept;
   }
 
-  connect(family, port, addr) {
-    console.log(`connecting with family=${family} port=${port} addr=${addr}`);
+  bind(family, port, addr) {
+    console.log(`binding with family=${family} port=${port} addr=${addr}`);
 
     this.#family = family;
     this.#port = port;
     this.#addr = addr;
+  }
+
+  listen(maxClients) {
+    this.#onListen(maxClients);
   }
 
   send(message, option) {
@@ -80,5 +112,9 @@ export class Socket {
 
   recv(option) {
     return this.#onRecv(option);
+  }
+
+  accept() {
+    return this.#onAccept();
   }
 }
