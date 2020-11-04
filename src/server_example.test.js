19ecd998cc1c5b6f816ac3fc467b26bed1a93a98
Felix Pojtinger <felix@pojtinger.com>
2020-11-04 21:57:02 +0000

    feat: Start implementation of `bind`, `listen` and `accept` bindings

--- src/server_example.test.js
+++ src/server_example.test.js
@@ -17,24 +17,69 @@ import { BerkeleySockets } from "./berkeley_sockets.js";
       berkeley_sockets_socket: (family, stream, option) =>
         berkeleySockets.socket(family, stream, option),
       berkeley_sockets_bind: (fd, addressPointer, addressLength) => {
-        console.log("berkeley_sockets_bind", fd, addressPointer, addressLength);
+        const sockaddrInMemory = new Uint8Array(
+          instance.exports.memory.buffer
+        ).slice(addressPointer, addressPointer + addressLength);
 
-        return -1;
+        const sin_family = sockaddrInMemory.slice(0, 2);
+        const sin_port = sockaddrInMemory.slice(2, 4);
+        const sin_addr = sockaddrInMemory.slice(4, 8);
+
+        const family = new Int32Array(sin_family)[0];
+        const port = berkeleySockets.htons(new Uint16Array(sin_port)[0]);
+        const addr = sin_addr.join(".");
+
+        const socket = berkeleySockets.getSocketByFileDescriptor(fd);
+
+        socket.bind(family, port, addr);
+
+        return 0;
       },
       berkeley_sockets_listen: (fd, maxClients) => {
-        console.log("berkeley_sockets_listen", fd, maxClients);
+        const socket = berkeleySockets.getSocketByFileDescriptor(fd);
 
-        return -1;
+        socket.listen(maxClients);
+
+        return 0;
       },
       berkeley_sockets_accept: (fd, addressPointer, addressLengthPointer) => {
-        console.log(
-          "berkeley_sockets_accept",
-          fd,
+        const socket = berkeleySockets.getSocketByFileDescriptor(fd);
+
+        const { family, port, address } = socket.accept();
+
+        console.log("berkeley_sockets_accept", fd, family, port, address);
+
+        const addressLength = new Int32Array(
+          new Uint8Array(instance.exports.memory.buffer).slice(
+            addressLengthPointer,
+            addressLengthPointer + 4
+          )
+        )[0];
+
+        const memory = new Uint8Array(instance.exports.memory.buffer);
+
+        const sin_family = berkeleySockets.getUint2(family);
+        const sin_port = berkeleySockets.getUint2(port);
+        const sin_addr = address.split(".").map((e) => new Uint8Array([e])[0]);
+
+        for (let i = 0; i < addressLength; i++) {
+          const index = addressPointer + i;
+
+          if (i >= 0 && i <= 2) {
+            memory[index] = sin_family[i];
+          } else if (i > 2 && i <= 4) {
+            memory[index - 1] = sin_port[i - 3];
+          } else if (i > 5 && i <= 8) {
+            // memory[index] = sin_addr[i - 5];
+          }
+        }
+
+        const mem2 = memory.slice(
           addressPointer,
-          addressLengthPointer
+          addressPointer + addressLength
         );
 
-        return -1;
+        return 0;
       },
       berkeley_sockets_send: (
         fd,
