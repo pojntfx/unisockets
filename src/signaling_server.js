3a189ab1f27b2dc447328fee6c22a7265d5cadfb
Felix Pojtinger <felix@pojtinger.com>
2020-11-05 23:19:48 +0000

    feat: Implement offer and answer passing

--- src/signaling_server.js
+++ src/signaling_server.js
@@ -1,17 +1,60 @@
+import EventEmitter from "events";
 import WebSocket from "ws";
 
 const PORT = 6999;
+const ANSWER = "answer";
+const OFFER = "offer";
 
-const wss = new WebSocket.Server({
+const broadcaster = new EventEmitter();
+const server = new WebSocket.Server({
   port: PORT,
 });
 
-wss.on("connection", (ws) => {
+server.on("connection", (connection) => {
   console.log("Client connected");
 
-  ws.on("message", (msg) => {
-    console.log("received", msg);
+  broadcaster.on("offer", (sid) => {
+    console.log("Sending offer");
 
-    ws.send(`Hey client! It is: ${new Date()}`);
+    connection.send(
+      JSON.stringify({
+        opcode: OFFER,
+        sid: sid,
+      })
+    );
+  });
+
+  broadcaster.on("answer", (sid) => {
+    console.log("Sending answer");
+
+    connection.send(
+      JSON.stringify({
+        opcode: ANSWER,
+        sid: sid,
+      })
+    );
+  });
+
+  connection.on("message", (msg) => {
+    const req = JSON.parse(msg);
+
+    switch (req.opcode) {
+      case OFFER: {
+        console.log("Received offer");
+
+        broadcaster.emit("offer", req.sid);
+
+        break;
+      }
+      case ANSWER: {
+        console.log("Received answer");
+
+        broadcaster.emit("answer", req.sid);
+
+        break;
+      }
+    }
   });
 });
+
+console.log(`Listening on port ${PORT}`);
