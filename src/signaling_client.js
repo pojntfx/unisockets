80e6e15cf9f54c84d445e7f4953f8ceb77ac4d3a
Felix Pojtinger <felix@pojtinger.com>
2020-11-06 00:18:40 +0000

    feat: Add basic SID exchange algorithm

--- src/signaling_client.js
+++ src/signaling_client.js
@@ -1,19 +1,74 @@
+import EventEmitter from "events";
 import WebSocket from "ws";
 
 const ADDRESS = "ws://localhost:6999";
 
-const ws = new WebSocket(ADDRESS);
+const OFFER_OPCODE = "offer";
+const ANSWER_OPCODE = "answer";
 
-ws.on("open", () => {
-  console.log("Server connected");
+const LOCAL_SID = Math.floor(Math.random() * 100000).toString();
 
-  ws.send(`Hey server! It is: ${new Date()}`);
-});
+const broadcaster = new EventEmitter();
+const client = new WebSocket(ADDRESS);
 
-ws.on("message", (msg) => {
-  console.log("received", msg);
-});
+client.on("open", () => {
+  console.log(`Connected to server ${ADDRESS}`);
 
-ws.on("close", () => {
-  console.log("Server disconnected");
+  broadcaster.on("offer", (sids) => {
+    console.log("Sending answer");
+
+    client.send(
+      JSON.stringify({
+        opcode: ANSWER_OPCODE,
+        sids: {
+          offer: sids.offer,
+          answer: LOCAL_SID,
+        },
+      })
+    );
+  });
+
+  broadcaster.on("answer", (sids) => {
+    console.log("Connecting to", sids.answer);
+  });
+
+  client.on("message", (msg) => {
+    const req = JSON.parse(msg);
+
+    switch (req.opcode) {
+      case OFFER_OPCODE: {
+        console.log("Received offer");
+
+        broadcaster.emit("offer", {
+          offer: req.sids.offer,
+        });
+
+        break;
+      }
+      case ANSWER_OPCODE: {
+        console.log("Received answer");
+
+        if (req.sids.offer !== LOCAL_SID && req.sids.answer !== LOCAL_SID) {
+          broadcaster.emit("answer", {
+            answer: req.sids.answer,
+          });
+        }
+
+        break;
+      }
+    }
+  });
+
+  setInterval(() => {
+    console.log("Sending offer");
+
+    client.send(
+      JSON.stringify({
+        opcode: OFFER_OPCODE,
+        sids: {
+          offer: LOCAL_SID,
+        },
+      })
+    );
+  }, 2500);
 });
