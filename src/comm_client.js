41c6013720a2b4e8280fa2edad44f86f3dc5ed09
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-11-07 22:03:42 +0000

    feat: Start connecting discovery and network connections

---
+++ src/comm_client.js
@@ -0,0 +1,52 @@
+import DiscoveryClient from "../lib/discovery_client.js";
+import NetworkInterface from "../lib/network_interface.js";
+
+const ADDRESS = "ws://localhost:6999";
+const LOCAL_SID = Math.floor(Math.random() * 100000).toString();
+
+const networkInterface = new NetworkInterface.Builder()
+  .setConfig({
+    iceServers: [
+      {
+        url: "stun:global.stun.twilio.com:3478?transport=udp",
+        urls: "stun:global.stun.twilio.com:3478?transport=udp",
+      },
+    ],
+  })
+  .setOnConnect((id, e) => console.log(id, "connected", e))
+  .setOnReceive((id, e) => console.log(id, "received", e))
+  .setOnDisconnect((id, e) => console.log(id, "disconnected", e))
+  .build();
+
+// TODO: Add connection addressing system (create ID with offer and transfer ID through signaling server)
+
+const discoveryClient = new DiscoveryClient.Builder()
+  .setAddress(ADDRESS)
+  .setGetOffer(async () => {
+    const connectionId = networkInterface.createConnection();
+
+    const connection = networkInterface.getConnectionById(connectionId);
+
+    const offer = await connection.getOffer();
+
+    console.log(`Offering ${offer.sdp}`);
+
+    return offer;
+  })
+  .setGetAnswer(async (offer) => {
+    console.log(`Answering ${offer.sdp}`);
+
+    const connectionId = networkInterface.createConnection();
+
+    const connection = networkInterface.getConnectionById(connectionId);
+
+    const answer = await connection.getAnswer(offer);
+
+    return answer.sdp;
+  })
+  .setOnAnswer((answer) => console.log(`Got answer ${answers.sdp}`))
+  .build();
+
+console.log(`Connecting to ${ADDRESS}`);
+
+discoveryClient.connect();
