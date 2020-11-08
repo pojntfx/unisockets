195ce13a4e66685575bf89fa09c8598f3a63f0a2
Felix Pojtinger <felix@pojtinger.com>
2020-11-08 15:11:59 +0000

    feat: Enable setting virtual address

--- src/comm_client.js
+++ src/comm_client.js
@@ -2,10 +2,9 @@ import DiscoveryClient from "../lib/discovery_client.js";
 import NetworkInterface from "../lib/network_interface.js";
 import { v4 } from "uuid";
 
-const ADDRESS = "ws://localhost:6999";
-const MESSAGE = `Hello peer! My message seed: ${v4()}`;
-
-console.log("Sending the following message to peers:", MESSAGE);
+const VIRTUAL_ADDRESS = `${v4()}:42069`;
+const REMOTE_ADDRESS = "ws://localhost:6999";
+const MESSAGE = `Hello peer from ${VIRTUAL_ADDRESS}`;
 
 const networkInterface = new NetworkInterface.Builder()
   .setConfig({
@@ -14,14 +13,39 @@ const networkInterface = new NetworkInterface.Builder()
         url: "stun:global.stun.twilio.com:3478?transport=udp",
         urls: "stun:global.stun.twilio.com:3478?transport=udp",
       },
+      {
+        url: "turn:global.turn.twilio.com:3478?transport=udp",
+        username:
+          "cd4dfc8127082a04b865f5607c031410726df919c4ced0f93619be0ed2b811b3",
+        urls: "turn:global.turn.twilio.com:3478?transport=udp",
+        credential: "h/sD5UftD1H2Tjlux/u6kqdqsIleXtEEZthVgY/BciA=",
+      },
+      {
+        url: "turn:global.turn.twilio.com:3478?transport=tcp",
+        username:
+          "cd4dfc8127082a04b865f5607c031410726df919c4ced0f93619be0ed2b811b3",
+        urls: "turn:global.turn.twilio.com:3478?transport=tcp",
+        credential: "h/sD5UftD1H2Tjlux/u6kqdqsIleXtEEZthVgY/BciA=",
+      },
+      {
+        url: "turn:global.turn.twilio.com:443?transport=tcp",
+        username:
+          "cd4dfc8127082a04b865f5607c031410726df919c4ced0f93619be0ed2b811b3",
+        urls: "turn:global.turn.twilio.com:443?transport=tcp",
+        credential: "h/sD5UftD1H2Tjlux/u6kqdqsIleXtEEZthVgY/BciA=",
+      },
     ],
   })
+  .setAddress(VIRTUAL_ADDRESS)
   .setOnConnect((id, e) => {
     console.log(id, "connected", e);
 
     const connection = networkInterface.getConnectionById(id);
 
-    connection.send(MESSAGE);
+    setInterval(
+      () => connection.send(`${MESSAGE}, it is now: ${Date.now()}`),
+      1000
+    );
   })
   .setOnReceive((id, e) => {
     console.log(id, "received", e);
@@ -32,7 +56,7 @@ const networkInterface = new NetworkInterface.Builder()
   .build();
 
 const discoveryClient = new DiscoveryClient.Builder()
-  .setAddress(ADDRESS)
+  .setAddress(REMOTE_ADDRESS)
   .setGetOffer(async (handler) => {
     const offerConnectionId = networkInterface.createConnection(null, handler);
     const offerConnection = networkInterface.getConnectionById(
@@ -76,6 +100,6 @@ const discoveryClient = new DiscoveryClient.Builder()
   })
   .build();
 
-console.log(`Connecting to ${ADDRESS}`);
+console.log(`Connecting to ${REMOTE_ADDRESS} as ${VIRTUAL_ADDRESS}`);
 
 discoveryClient.connect();
