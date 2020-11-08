370ed674e940a1efad13afd92c93b9fc8d29bcd9
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-11-08 18:32:04 +0000

    feat: Start connection of new Berkeley socket API to WASM

--- src/client_example.test.js
+++ src/client_example.test.js
@@ -1,79 +1,38 @@
 import fs from "fs";
 import { WASI } from "wasi";
-import { BerkeleySockets } from "./berkeley_sockets.js";
+import BerkeleySocketManager from "../lib/berkeley_socket_manager.js";
+import EventEmitter from "events";
+
+const BINARY_PATH = "./src/client_example.wasm";
 
 (async () => {
   const wasi = new WASI();
+  const berkeleySocketManager = new BerkeleySocketManager.Builder()
+    .setGetConnection((family, port, addr) => {
+      console.log(`Getting connection for ${family} ${addr}:${port}`);
 
-  const wasm = await WebAssembly.compile(
-    fs.readFileSync("./src/client_example.wasm")
+      // TODO: Return connection here
+    })
+    .setGetReceiver((family, port, addr) => {
+      console.log(`Getting receiver for ${family} ${addr}:${port}`);
+
+      const receiverBroadcaster = new EventEmitter();
+
+      // TODO: Connect to or return connection receiver
+
+      return receiverBroadcaster;
+    })
+    .build();
+
+  const instance = await WebAssembly.instantiate(
+    await WebAssembly.compile(fs.readFileSync(BINARY_PATH)),
+    {
+      wasi_snapshot_preview1: wasi.wasiImport,
+      env: berkeleySocketManager.getImports(),
+    }
   );
 
-  const berkeleySockets = new BerkeleySockets();
-
-  const instance = await WebAssembly.instantiate(wasm, {
-    wasi_snapshot_preview1: wasi.wasiImport,
-    env: {
-      berkeley_sockets_socket: (family, stream, option) =>
-        berkeleySockets.socket(family, stream, option),
-      berkeley_sockets_connect: (fd, addressPointer, addressLength) => {
-        const sockaddrInMemory = new Uint8Array(
-          instance.exports.memory.buffer
-        ).slice(addressPointer, addressPointer + addressLength);
-
-        const sin_family = sockaddrInMemory.slice(0, 2);
-        const sin_port = sockaddrInMemory.slice(2, 4);
-        const sin_addr = sockaddrInMemory.slice(4, 8);
-
-        const family = new Int32Array(sin_family)[0];
-        const port = berkeleySockets.htons(new Uint16Array(sin_port)[0]);
-        const addr = sin_addr.join(".");
-
-        const socket = berkeleySockets.getSocketByFileDescriptor(fd);
-
-        socket.connect(family, port, addr);
-
-        return 0;
-      },
-      berkeley_sockets_send: (
-        fd,
-        messagePointer,
-        messagePointerLength,
-        option
-      ) => {
-        const message = new Uint8Array(instance.exports.memory.buffer).slice(
-          messagePointer,
-          messagePointer + messagePointerLength
-        );
-
-        const socket = berkeleySockets.getSocketByFileDescriptor(fd);
-
-        socket.send(message, option);
-
-        return message.length;
-      },
-      berkeley_sockets_recv: (
-        fd,
-        messagePointer,
-        messagePointerLength,
-        option
-      ) => {
-        const socket = berkeleySockets.getSocketByFileDescriptor(fd);
-
-        const message = socket.recv(option);
-
-        const memory = new Uint8Array(instance.exports.memory.buffer);
-        message.forEach((messagePart, index) => {
-          // Don't write over the boundary
-          if (index <= messagePointerLength) {
-            memory[messagePointer + index] = messagePart;
-          }
-        });
-
-        return message.length;
-      },
-    },
-  });
+  berkeleySocketManager.setMemory(instance.exports.memory);
 
   wasi.start(instance);
 })();
