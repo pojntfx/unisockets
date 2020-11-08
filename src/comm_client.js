2661c307c90ea6ca011719ae732b53a419efb1c5
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-11-08 14:06:34 +0000

    feat: Connect candidate handlers

--- src/comm_client.js
+++ src/comm_client.js
@@ -1,7 +1,11 @@
 import DiscoveryClient from "../lib/discovery_client.js";
 import NetworkInterface from "../lib/network_interface.js";
+import EventEmitter from "events";
 
 const ADDRESS = "ws://localhost:6999";
+
+const candidateBus = new EventEmitter();
+candidateBus.setMaxListeners(10000);
 
 const networkInterface = new NetworkInterface.Builder()
   .setConfig({
@@ -20,17 +24,18 @@ const networkInterface = new NetworkInterface.Builder()
     connection.send("Hello!");
   })
   .setOnReceive((id, e) => {
-    // TODO: Fix `ondatachannel` not being called
-
     console.log(id, "received", e);
   })
   .setOnDisconnect((id, e) => {
     console.log(id, "disconnected", e);
   })
-  .setOnCandidate((id, e) => {
-    // TODO: Transfer candidate via discovery client and call `acceptCandidate` for connection with ID
+  .setOnCandidate((connectionId, candidate) => {
+    candidateBus.emit("candidate", {
+      connectionId,
+      candidate,
+    });
 
-    console.log(id, "candidate", e);
+    console.log(connectionId, "candidate", candidate);
   })
   .build();
 
@@ -69,7 +74,21 @@ const discoveryClient = new DiscoveryClient.Builder()
 
     offerConnection.acceptAnswer(answer);
   })
+  .setOnCandidate(({ connectionId, candidate }) => {
+    console.log(`Got candidate ${candidate}`);
+
+    const connection = networkInterface.getConnectionById(connectionId);
+
+    connection && connection.acceptCandidate(candidate);
+  })
   .build();
+
+candidateBus.on("candidate", (candidate) => {
+  discoveryClient.sendCandidate({
+    connectionId: candidate.connectionId,
+    candidate: candidate.candidate,
+  });
+});
 
 console.log(`Connecting to ${ADDRESS}`);
 
