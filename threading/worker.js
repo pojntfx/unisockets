c91d1da9012b848deaed24494a8347e924f1ab4c
Felix Pojtinger <felix@pojtinger.com>
2020-11-10 22:21:15 +0000

    refactor: Communicate in worker with event emitters

--- threading/worker.js
+++ threading/worker.js
@@ -1,11 +1,17 @@
 import { Observable, Subject } from "threads/observable";
 import { expose } from "threads/worker";
+import EventEmitter from "events";
+
+const threadConnection = new EventEmitter();
 
 const subject = new Subject();
 
+threadConnection.on("message", (message) => subject.next(message));
+
 const worker = {
   send(message) {
-    subject.next(
+    threadConnection.emit(
+      "message",
       new TextEncoder().encode(
         "You've send: " + new TextDecoder().decode(message)
       )
