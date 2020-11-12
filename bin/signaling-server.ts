429d109bdfdb9dc5f9fd894b51327f9e282eb1de
Felix Pojtinger <felix@pojtinger.com>
2020-11-12 21:50:12 +0000

    feat: Implement acknowledge and gone

--- bin/signaling-server.ts
+++ bin/signaling-server.ts
@@ -1,23 +1,5 @@
-import WebSocket, { Server } from "ws";
 import yargs from "yargs";
-import { v4 } from "uuid";
-import { createLogger, transports } from "winston";
-
-const log = createLogger({ transports: new transports.Console() });
-
-enum ESIGNALING_SERVER_OPCODES {
-  ACKNOWLEDGED,
-}
-
-interface IOperation {
-  opcode: ESIGNALING_SERVER_OPCODES;
-}
-
-interface IOperationAcknowledgement extends IOperation {
-  data: {
-    id: string;
-  };
-}
+import { SignalingServer } from "../lib/signaling-server";
 
 const { laddr } = yargs(process.argv.slice(2)).options({
   laddr: {
@@ -25,33 +7,9 @@ const { laddr } = yargs(process.argv.slice(2)).options({
   },
 }).argv;
 
-const server = new Server({
-  host: laddr.split(":")[0],
-  port: parseInt(laddr.split(":")[1]),
-});
+const server = new SignalingServer(
+  laddr.split(":")[0],
+  parseInt(laddr.split(":")[1])
+);
 
-const clients = new Map<string, WebSocket>();
-
-server.on("connection", (client) => {
-  const id = v4();
-  clients.set(id, client);
-
-  const acknowledgement: IOperationAcknowledgement = {
-    opcode: ESIGNALING_SERVER_OPCODES.ACKNOWLEDGED,
-    data: {
-      id,
-    },
-  };
-  client.send(JSON.stringify(acknowledgement));
-  log.info("Client connected", { id });
-
-  client.on("message", (message) => {
-    client.send(`You've sent: ${message}`);
-  });
-
-  client.on("close", () => {
-    clients.delete(id);
-
-    log.info("Client disconnected", { id });
-  });
-});
+server.open();
