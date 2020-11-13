7c544dd7715c3d70f7147ead698cd42d0897547c
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-11-13 21:19:25 +0000

    feat: Send and relay answers when receiving offer

--- lib/signaling/operations/operation.ts
+++ lib/signaling/operations/operation.ts
@@ -1,14 +1,20 @@
 import { IGoneData } from "./gone";
 import { IAcknowledgementData } from "./acknowledgement";
 import { IOfferData } from "./offer";
+import { IAnswerData } from "./answer";
 
 export enum ESIGNALING_OPCODES {
   GONE,
   ACKNOWLEDGED,
   OFFER,
+  ANSWER,
 }
 
-export type TSignalingData = IGoneData | IAcknowledgementData | IOfferData;
+export type TSignalingData =
+  | IGoneData
+  | IAcknowledgementData
+  | IOfferData
+  | IAnswerData;
 
 export interface ISignalingOperation<T> {
   opcode: ESIGNALING_OPCODES;
