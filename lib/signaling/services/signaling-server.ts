fe2e69e252d72dbe10f78dd11f2125ea8276222b
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-11-13 00:28:52 +0000

    feat: Enable sending offer, add automatic reconnect

--- lib/signaling/services/signaling-server.ts
+++ lib/signaling/services/signaling-server.ts
@@ -1,8 +1,15 @@
 import { v4 } from "uuid";
 import WebSocket, { Server } from "ws";
+import { ClientDoesNotExistError } from "../errors/client-does-not-exist";
+import { UnimplementedOperationError } from "../errors/unimplemented-operation";
 import { Acknowledgement } from "../operations/acknowledgement";
 import { Gone } from "../operations/gone";
-import { ISignalingOperation, TSignalingData } from "../operations/operation";
+import { IOfferData, Offer } from "../operations/offer";
+import {
+  ESIGNALING_OPCODES,
+  ISignalingOperation,
+  TSignalingData,
+} from "../operations/operation";
 import { Service } from "./service";
 
 export class SignalingServer extends Service {
@@ -46,20 +53,53 @@ export class SignalingServer extends Service {
   }
 
   private async registerGoodbye(id: string) {
-    this.clients.get(id)?.on("close", () => {
-      this.clients.delete(id);
+    const client = this.clients.get(id);
 
-      this.clients.forEach(
-        async (client) => await this.send(client, new Gone({ id }))
-      );
+    if (client) {
+      client.on("close", () => {
+        this.clients.delete(id);
 
-      this.logger.info("Disconnected", { id });
-    });
+        this.clients.forEach(
+          async (client) => await this.send(client, new Gone({ id }))
+        );
+
+        this.logger.info("Disconnected", { id });
+      });
+    } else {
+      throw new ClientDoesNotExistError();
+    }
   }
 
   private async handleOperation(
     operation: ISignalingOperation<TSignalingData>
   ) {
     this.logger.debug("Handling Operation", operation);
+
+    switch (operation.opcode) {
+      case ESIGNALING_OPCODES.OFFER: {
+        const data = operation.data as IOfferData;
+
+        this.clients.forEach(async (client, id) => {
+          id !== data.id &&
+            (await this.send(
+              client,
+              new Offer({
+                id,
+                offer: data.offer,
+              })
+            ));
+        });
+
+        this.logger.info("Offer", data);
+
+        break;
+      }
+
+      // TODO: Handle answers
+
+      default: {
+        throw new UnimplementedOperationError(operation.opcode);
+      }
+    }
   }
 }
