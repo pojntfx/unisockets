84bdf1f4af5217b0625377e69d8493da2034ab7a
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-11-16 22:59:02 +0000

    feat: Add stub for transporter

---
+++ lib/transport/transporter.ts
@@ -0,0 +1,25 @@
+import { v4 } from "uuid";
+
+export class Transporter {
+  async open() {
+    return v4();
+  }
+
+  async handleOffer(id: string, offer: string) {
+    return v4();
+  }
+
+  async handleAnswer(
+    id: string,
+    answer: string,
+    handleCandidate: (candidate: string) => Promise<void>
+  ) {
+    await handleCandidate(v4());
+    await handleCandidate(v4());
+    await handleCandidate(v4());
+
+    return v4();
+  }
+
+  async handleCandidate(id: string, candidate: string) {}
+}
