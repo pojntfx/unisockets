2b824e67a1358b1ea5cf861d22b37e53bfd56e82
Jakob Waibel <jakob.waibel@web.de>
2020-11-20 10:13:10 +0100

    fix: Pow() function undefined for e = 0

--- lib/examples/go/old/chudnovsky-big.go
+++ lib/examples/go/old/chudnovsky-big.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-	"math/big"
 	"math"
+	"math/big"
 )
 
 func main() {
@@ -22,70 +22,80 @@ func main() {
 	fmt.Println(result)
 }
 
+// 0 hoch 0 ist nicht definiert
 func sumElement(i *big.Float) *big.Float {
-	return Pow(i,2);
+	iAsUint64, _ := i.Uint64()
+	fmt.Println(iAsUint64)
+	// Bis hier stimmt alles
+	return Pow(big.NewFloat(-1), iAsUint64)
+
 }
 
 func Pow(a *big.Float, e uint64) *big.Float {
-    result := Zero().Copy(a)
-    for i:=uint64(0); i<e-1; i++ {
-        result = Mul(result, a)
-    }
-    return result
+	result := Zero().Copy(a)
+	if e == 0 {
+		return big.NewFloat(1)
+	}
+	for i := uint64(0); i < e-1; i++ {
+		result = Mul(result, a)
+	}
+	return result
 }
 
 func Root(a *big.Float, n uint64) *big.Float {
-    limit := Pow(New(2), 256)
-    n1 := n-1
-    n1f, rn := New(float64(n1)), Div(New(1.0), New(float64(n)))
-    x, x0 := New(1.0), Zero()
-    _ = x0
-    for {
-        potx, t2 := Div(New(1.0), x), a
-        for b:=n1; b>0; b>>=1 {
-            if b&1 == 1 {
-                t2 = Mul(t2, potx)
-            }
-            potx = Mul(potx, potx)
-        }
-        x0, x = x, Mul(rn, Add(Mul(n1f, x), t2) )
-        if Lesser(Mul(Abs(Sub(x, x0)), limit), x) { break }
-    }
-    return x
+	limit := Pow(New(2), 256)
+	n1 := n - 1
+	n1f, rn := New(float64(n1)), Div(New(1.0), New(float64(n)))
+	x, x0 := New(1.0), Zero()
+	_ = x0
+	for {
+		potx, t2 := Div(New(1.0), x), a
+		for b := n1; b > 0; b >>= 1 {
+			if b&1 == 1 {
+				t2 = Mul(t2, potx)
+			}
+			potx = Mul(potx, potx)
+		}
+		x0, x = x, Mul(rn, Add(Mul(n1f, x), t2))
+		if Lesser(Mul(Abs(Sub(x, x0)), limit), x) {
+			break
+		}
+	}
+	return x
 }
 
 func Abs(a *big.Float) *big.Float {
-    return Zero().Abs(a)
+	return Zero().Abs(a)
 }
 
 func New(f float64) *big.Float {
-    r := big.NewFloat(f)
-    r.SetPrec(256)
-    return r
+	r := big.NewFloat(f)
+	r.SetPrec(256)
+	return r
 }
 
 func Div(a, b *big.Float) *big.Float {
-    return Zero().Quo(a, b)
+	return Zero().Quo(a, b)
 }
 
 func Zero() *big.Float {
-    r := big.NewFloat(0.0)
-    r.SetPrec(256)
-    return r
+	r := big.NewFloat(0.0)
+	r.SetPrec(256)
+	return r
 }
 
 func Mul(a, b *big.Float) *big.Float {
-    return Zero().Mul(a, b)
+	return Zero().Mul(a, b)
 }
 
 func Add(a, b *big.Float) *big.Float {
-    return Zero().Add(a, b)
+	return Zero().Add(a, b)
 }
 
 func Sub(a, b *big.Float) *big.Float {
-    return Zero().Sub(a, b)
+	return Zero().Sub(a, b)
 }
 
 func Lesser(x, y *big.Float) bool {
-    return x.Cmp(y) == -1
+	return x.Cmp(y) == -1
 }
