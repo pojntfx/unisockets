f63f48603d311bc492d1f10644db4fa3aef79439
Jakob Waibel <jakob.waibel@web.de>
2020-11-23 12:12:46 +0100

    feat: Distribute data

--- lib/examples/go/old/concurrent-server.go
+++ lib/examples/go/old/concurrent-server.go
@@ -19,8 +19,14 @@ type Messenger struct {
 	message string
 }
 
+type OutputJSON struct {
+	ionCount   int       `json:"ionCount"`
+	inputArray []float64 `json:"inputArray"`
+}
+
 var (
-	count int
+	count      int
+	inputArray []float64
 )
 
 func main() {
@@ -49,6 +55,8 @@ func (s TCPServer) open() error {
 
 	m := messenger.New(0, false)
 
+	inputArray = []float64{1, 1, 3}
+
 	go startCalc(m)
 
 	for {
@@ -71,7 +79,7 @@ func startCalc(m *messenger.Messenger) {
 		fmt.Println(username)
 
 		var i interface{}
-		i = fmt.Sprintf("{ionCount: %v}", count)
+		i = fmt.Sprintf(`{"ionCount": %v, "inputArray": %v}`, count, inputArray)
 
 		m.Broadcast(i)
 
@@ -97,7 +105,27 @@ func handleConnection(conn net.Conn, myCount int, m *messenger.Messenger) {
 
 		fmt.Println(msg)
 
-		_, err2 := conn.Write([]byte(msg.(string)))
+		// append myCount to Message
+		// Convert string to json
+		// create new json
+		// rawIn := json.RawMessage(msg.(string))
+
+		// bytes, err := rawIn.MarshalJSON()
+		// if err != nil {
+		// 	log.Fatal(err)
+		// }
+
+		// var j OutputJSON
+		// err = json.Unmarshal(bytes, &j)
+		// if err != nil {
+		// 	log.Fatal(err)
+		// }
+
+		//ionCount := j.ionCount
+		//inputArray := j.inputArray
+		// myCount
+
+		_, err2 := conn.Write([]byte(fmt.Sprintf(`{"ionCount": %v, "inputArray": %v, "myCount": %v}`, count, inputArray, myCount)))
 		if err2 != nil {
 			log.Fatal(err2)
 		}
