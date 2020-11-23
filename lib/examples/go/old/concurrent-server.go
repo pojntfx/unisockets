61de6f14234dc0a830d46bf6a3589a4ec883df95
Jakob Waibel <jakob.waibel@web.de>
2020-11-23 10:45:28 +0100

    feat: Add ion data distribution skeleton

--- lib/examples/go/old/concurrent-server.go
+++ lib/examples/go/old/concurrent-server.go
@@ -19,6 +19,10 @@ type Messenger struct {
 	message string
 }
 
+var (
+	count int
+)
+
 func main() {
 	tcpServer := newTCPServer("0.0.0.0:3333")
 
@@ -32,8 +36,6 @@ func newTCPServer(laddr string) *TCPServer {
 }
 
 func (s TCPServer) open() error {
-	ionCount := 0
-	//inputArray := [3]int{1, 1, 3}
 
 	tcpAddr, err := net.ResolveTCPAddr("tcp", s.laddr)
 	if err != nil {
@@ -47,8 +49,6 @@ func (s TCPServer) open() error {
 
 	m := messenger.New(0, false)
 
-	//messages := make(chan string)
-
 	go startCalc(m)
 
 	for {
@@ -56,10 +56,8 @@ func (s TCPServer) open() error {
 		if err != nil {
 			log.Fatal(err)
 		}
-
-		ionCount++
-
-		go handleConnection(conn, ionCount, m)
+		count++
+		go handleConnection(conn, count, m)
 	}
 
 }
@@ -72,15 +70,8 @@ func startCalc(m *messenger.Messenger) {
 		username, _ := reader.ReadString('\n')
 		fmt.Println(username)
 
-		// Instead of printing username, take array of all users so far and put the information for each node to their channel
-		// Code receiving channel
-
-		// send jsonString with necessary information
-		// Maybe just notify all goroutines and get the data from "public variables" afterwards
-		//messages <- "Hi"
-
 		var i interface{}
-		i = "Hello World"
+		i = fmt.Sprintf("{ionCount: %v}", count)
 
 		m.Broadcast(i)
 
@@ -98,7 +89,6 @@ func handleConnection(conn net.Conn, myCount int, m *messenger.Messenger) {
 			}
 		}
 
-		// Print "Connected"
 		fmt.Println(string(input[0:n]))
 
 		data, err := m.Sub()
