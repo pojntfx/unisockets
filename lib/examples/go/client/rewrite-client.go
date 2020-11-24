29453652588059c458e3497509a011e5da4cfe71
Jakob Waibel <jakob.waibel@web.de>
2020-11-24 20:32:31 +0100

    feat: Add decodeJSONSumResult and decodeJSONSumInput to rewrite

--- lib/examples/go/client/rewrite-client.go
+++ lib/examples/go/client/rewrite-client.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"math"
 	"net"
 )
 
+// DecodeJSONSumInput decodes JSON sum input
+type DecodeJSONSumInput struct {
+	InputArray []float64 `json:"inputArray"`
+	IonCount   int       `json:"ionCount"`
+	MyCount    int       `json:"myCount"`
+}
+
 func main() {
+	var jsonSumInput [512]byte
 
 	tcpAddr, err := net.ResolveTCPAddr("tcp", "0.0.0.0:3333")
 	checkError(err)
@@ -14,7 +24,19 @@ func main() {
 	conn, err := net.DialTCP("tcp", nil, tcpAddr)
 	checkError(err)
 
-	_, err = conn.Write([]byte(`{"inputArray": [1,1,3], "ionCount": 3, "myCount": 0}`))
+	_, err = conn.Write([]byte(`Connected`))
+	checkError(err)
+
+	n, err := conn.Read(jsonSumInput[0:])
+	checkError(err)
+
+	a := decodeJSONSumInput(string(jsonSumInput[0:n]))
+
+	fmt.Println(a.IonCount)
+
+	// process
+
+	_, err = conn.Write([]byte(`{"sumResult": [2.7, 2.7, 20.0]}`))
 	checkError(err)
 
 }
@@ -31,4 +53,18 @@ func checkError(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func decodeJSONSumInput(input string) DecodeJSONSumInput {
+	rawIn := json.RawMessage(input)
+
+	bytes, err := rawIn.MarshalJSON()
+	checkError(err)
+
+	var d DecodeJSONSumInput
+
+	err = json.Unmarshal(bytes, &d)
+	checkError(err)
+
+	return d
 }
