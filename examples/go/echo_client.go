2f2ce403b26fd905cabc0eafec4a718e2d8e8d3e
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-11-24 15:28:55 +0000

    feat: Add remote address construction

--- examples/go/echo_client.go
+++ examples/go/echo_client.go
@@ -2,10 +2,34 @@ package main
 
 /*
 #include <berkeley_sockets.h>
+
+typedef struct sockaddr_in sockaddr_in;
+typedef struct in_addr in_addr;
 */
 import "C"
-import "fmt"
+import (
+	"encoding/binary"
+	"fmt"
+)
+
+const (
+	remotePort = 1234
+)
+
+var (
+	remoteHost = []byte{127, 0, 0, 1}
+)
 
 func main() {
-	fmt.Println(C.socket(C.PF_INET, C.SOCK_STREAM, 0))
+	remoteAddress := C.sockaddr_in{
+		sin_family: C.PF_INET,
+		sin_port:   C.htons(remotePort),
+		sin_addr: C.in_addr{
+			s_addr: C.uint(binary.LittleEndian.Uint32(remoteHost)),
+		},
+	}
+
+	remoteSocket := C.socket(C.PF_INET, C.SOCK_STREAM, 0)
+
+	fmt.Println(remoteSocket, remoteAddress)
 }
