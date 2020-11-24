2a3d127ac62148c96e8979182beef0080e4a243e
Jakob Waibel <jakob.waibel@web.de>
2020-11-24 20:31:55 +0100

    feat: Add decodeJSONSumResult and decodeJSONSumInput to rewrite

--- lib/examples/go/server/rewrite-server.go
+++ lib/examples/go/server/rewrite-server.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net"
 )
+
+type JSONSum struct {
+	InputArray []float64 `json:"inputArray"`
+	IonCount   int       `json:"ionCount"`
+	MyCount    int       `json:"myCount"`
+}
 
 func main() {
 	tcpAddr, err := net.ResolveTCPAddr("tcp", "0.0.0.0:3333")
@@ -29,6 +36,9 @@ func handleConnection(conn *net.TCPConn) {
 
 	fmt.Println(string(input[0:n]))
 
+	a := encodeJSONSum(string(input[0:n]))
+	fmt.Println(a.IonCount)
+
 }
 
 func checkError(err error) {
@@ -36,3 +46,23 @@ func checkError(err error) {
 		log.Fatal(err)
 	}
 }
+
+func encodeJSONSum(input string) JSONSum {
+	rawIn := json.RawMessage(input)
+
+	bytes, err := rawIn.MarshalJSON()
+	checkError(err)
+
+	var j JSONSum
+
+	err = json.Unmarshal(bytes, &j)
+	checkError(err)
+
+	return j
+}
+
+func decodeJSONSum() {}
+
+func encodeJSONResult() {}
+
+func decodeJSONResult() {}
