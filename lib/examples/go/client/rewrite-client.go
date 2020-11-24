840ef761fbccc3a4afa3d90ec94f65ef258134b6
Jakob Waibel <jakob.waibel@web.de>
2020-11-24 22:36:40 +0100

    feat: Add missing JSON encoders and decoders to rewrite

--- lib/examples/go/client/rewrite-client.go
+++ lib/examples/go/client/rewrite-client.go
@@ -21,8 +21,21 @@ type EncodeJSONSumResult struct {
 	MyCount   int       `json:"myCount"`
 }
 
+type DecodeJSONSoftmaxInput struct {
+	InputArray []float64 `json:"inputArray"`
+	IonCount   int       `json:"ionCount"`
+	MyCount    int       `json:"myCount"`
+	Sum        int       `json:"sum"`
+}
+
+type EncodeJSONSoftmaxResult struct {
+	SoftmaxResult []float64 `json:"softmaxResult"`
+	MyCount       int       `json:"myCount"`
+}
+
 func main() {
 	var jsonSumInput [512]byte
+	var jsonSoftmaxInput [512]byte
 	var jsonSumResult = []float64{2.7, 2.7, 20.0}
 
 	tcpAddr, err := net.ResolveTCPAddr("tcp", "0.0.0.0:3333")
@@ -47,9 +60,29 @@ func main() {
 
 	fmt.Println(string(bytes))
 
-	_, err = conn.Write([]byte(bytes))
+	_, err = conn.Write(bytes)
 	checkError(err)
 
+	// Read actual jsonSoftmaxInput
+	//jsonSoftmaxInput := `{"inputArray": [1,1,3], "ionCount": 3, "myCount": 0, "sum": 25}`
+	o, err := conn.Read(jsonSoftmaxInput[0:])
+	checkError(err)
+
+	b := decodeJSONSoftmaxInput(string(jsonSoftmaxInput[0:o]))
+
+	fmt.Println(b.InputArray)
+
+	//  calculate jsonSoftmaxResult
+
+	var jsonSoftmaxResult = []float64{0.1, 0.1, 0.78}
+
+	bytes2 := encodeJSONSoftmaxResult(EncodeJSONSoftmaxResult{jsonSoftmaxResult, 0})
+
+	_, err = conn.Write(bytes2)
+	checkError(err)
+
+	fmt.Println(string(bytes2))
+	fmt.Println(b)
 }
 
 func softmaxSum(input float64) float64 {
@@ -67,6 +100,7 @@ func checkError(err error) {
 }
 
 func decodeJSONSumInput(input string) DecodeJSONSumInput {
+
 	rawIn := json.RawMessage(input)
 
 	bytes, err := rawIn.MarshalJSON()
@@ -81,6 +115,29 @@ func decodeJSONSumInput(input string) DecodeJSONSumInput {
 }
 
 func encodeJSONSumResult(s EncodeJSONSumResult) []byte {
+
+	bytes, err := json.Marshal(s)
+	checkError(err)
+
+	return bytes
+}
+
+func decodeJSONSoftmaxInput(input string) DecodeJSONSoftmaxInput {
+
+	rawIn := json.RawMessage(input)
+
+	bytes, err := rawIn.MarshalJSON()
+	checkError(err)
+
+	var d DecodeJSONSoftmaxInput
+
+	err = json.Unmarshal(bytes, &d)
+	checkError(err)
+
+	return d
+}
+
+func encodeJSONSoftmaxResult(s EncodeJSONSoftmaxResult) []byte {
 
 	bytes, err := json.Marshal(s)
 	checkError(err)
