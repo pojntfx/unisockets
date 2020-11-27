8d88dddea79303ca623abfe1869a3366c77021ba
Felix Pojtinger <felix@pojtinger.com>
2020-11-27 18:59:16 +0000

    feat: Add nested accept/receive example

--- examples/tinygo/cmd/async-echo-server/main.go
+++ examples/tinygo/cmd/async-echo-server/main.go
@@ -2,21 +2,43 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 var (
-	acceptChan = make(chan int)
+	acceptChan   = make(chan int)
+	receiveChans = make(map[int]chan int)
 )
 
+var receiveChanLock sync.Mutex
+
 func triggerAccept()
+
+func triggerReceive(fd int)
 
 //export resolveAccept
 func resolveAccept(fd int) {
 	go func(innerFd int) {
-		fmt.Println("Resolved")
-
 		acceptChan <- innerFd
+	}(fd)
+}
+
+//export resolveReceive
+func resolveReceive(fd int, msg int) {
+	go func(innerFd int) {
+		matchingChan, ok := receiveChans[innerFd]
+		if !ok {
+			matchingChan = make(chan int)
+
+			receiveChanLock.Lock()
+
+			receiveChans[innerFd] = matchingChan
+
+			receiveChanLock.Unlock()
+		}
+
+		matchingChan <- msg
 	}(fd)
 }
 
@@ -26,26 +48,47 @@ func waitAccept() int {
 	return res
 }
 
+func waitReceive(fd int) int {
+	matchingChan, ok := receiveChans[fd]
+	if !ok {
+		matchingChan = make(chan int)
+
+		receiveChanLock.Lock()
+
+		receiveChans[fd] = matchingChan
+
+		receiveChanLock.Unlock()
+	}
+
+	res := <-matchingChan
+
+	return res
+}
+
 func main() {
 	for {
-		fmt.Println("Accepting")
-
 		triggerAccept()
 
-		fmt.Println("Triggered")
-
 		go func() {
-			fmt.Println("Waiting")
-
 			fd := waitAccept()
 
 			for {
-				fmt.Println("Waited", fd)
+				triggerReceive(fd)
 
-				time.Sleep(time.Millisecond * 500)
+				doneChan := make(chan struct{})
+
+				go func(innerFd int) {
+					msg := waitReceive(innerFd)
+
+					fmt.Printf("Received from %v: %v\n", innerFd, msg)
+
+					doneChan <- struct{}{}
+				}(fd)
+
+				<-doneChan
 			}
 		}()
 
-		time.Sleep(time.Second * 2)
+		time.Sleep(time.Second * 2) // TODO: Replace this exit preventer (https://medium.com/swlh/getting-started-with-webassembly-and-go-by-building-an-image-to-ascii-converter-dea10bdf71f6)
 	}
 }
