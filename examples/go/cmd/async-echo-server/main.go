4b595e7af8b28e72cf44fc99479f31831bfdcb4e
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-11-27 21:08:38 +0000

    feat: Start async interop layer implementation

--- examples/go/cmd/async-echo-server/main.go
+++ examples/go/cmd/async-echo-server/main.go
@@ -1,7 +1,41 @@
 package main
 
-import "log"
+import (
+	"log"
+	"syscall/js"
+)
+
+var (
+	berkeley_sockets = js.Global().Get("berkeleySockets")
+)
+
+const (
+	PF_INET     = 2
+	SOCK_STREAM = 1
+)
+
+func socket(socketDomain int, socketType int, socketProtocol int) int {
+	rvChan := make(chan int)
+
+	go berkeley_sockets.Call("berkeley_sockets_socket", socketDomain, socketType, socketProtocol).Call("then", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		rvChan <- args[0].Int()
+
+		return nil
+	}))
+
+	rv := <-rvChan
+
+	return rv
+}
 
 func main() {
-	log.Println("Hello, world!")
+	// Create address
+	serverSocket := socket(PF_INET, SOCK_STREAM, 0)
+	if serverSocket == -1 {
+		log.Fatal("[ERROR] Could not create socket:", serverSocket)
+	}
+
+	log.Println("fd", serverSocket)
+
+	select {}
 }
